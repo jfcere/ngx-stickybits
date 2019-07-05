eabc7a47fc60abe6ef8fb486a7ff0212ffae1832
Jean-Francois Cere <jfcere@sherweb.com>
2019-07-04 20:52:15 -0400

    Update Angular 8 (#1)

--- lib/src/stickybits.directive.spec.ts
+++ lib/src/stickybits.directive.spec.ts
@@ -2,7 +2,7 @@ import { StickybitsDirective } from './stickybits.directive';
 
 describe('StickybitsDirective', () => {
   it('should create an instance', () => {
-    const directive = new StickybitsDirective();
+    const directive = new StickybitsDirective(null, null, null);
     expect(directive).toBeTruthy();
   });
 });
